0ed7e444cb4f93abc1a6eca690461c1ece7f6f2e
gjpineda <gjpineda@live.com>
2023-02-16 23:06:46 -0800

    optimized shows.js script & adjusted hero height

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,10 +1,10 @@
 // The square brackets signify an array. We are creating a const variable called "cards".
 // and we are setting it to an array.
 // for each array item, we make an object. curly braces signify object.
-// each object, has the property date, venue, and location.
+// each object, has the property called date, venue, and location.
 const cards = [
   {
-    date: new Date("Sept 06 2021"),
+    date: new Date(1630911600000),
     venue: "Ronald Lane",
     location: "San Francisco, CA",
   },
@@ -36,34 +36,41 @@ const cards = [
 ];
 
 // the reason why we store this in a variable is so we can access it later. "tickets_element" is a VARIABLE.
-// const variable called tickets_element and we're setting this variable into
-// HTML with the ID #tickets.
+// we are storing the HTML element with the ID "#tickets" into tickets_element VARIABLE.
+// the document.querySelector selects an HTML element with ID "#tickets" then
+// stores it into the tickets_element VARIABLE.
+// querySelector is a general selector, others include getElementByID, get ElementByClass, etc.
+// a hashtag makes something an ID.
 const tickets_element = document.querySelector("#tickets");
 
-// creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
-// the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
-for (let index = 0; index < cards.length; index++) {
-  // for everyloop, we are creating a div HTML element and we're
-  // storing it in a variable called card_element, dateHeading_element, etc.
+// We created a reusable function that requires a parameter called "card".
+// "card" is expected to be an object with the properties: Date, Venue, and Location.
+function createCard(card) {
+  // everytime this function is called, we are creating an element called "div, h2, p, etc."
   const card_element = document.createElement("div");
-  const dateHeading_element = document.createElement("h2");
-  const dateData_element = document.createElement("p");
-  const venueHeading_element = document.createElement("h2");
-  const venueData_element = document.createElement("p");
-  const locationHeading_element = document.createElement("h2");
-  const locationData_element = document.createElement("p");
-  const cardButton_element = document.createElement("button");
-  const underline_element = document.createElement("hr");
 
-  // sets the value of each HTML element to a specific value.
-  // for example: from this: <h2></h2> ------> to this: <h2>Date</h2>
-  dateHeading_element.innerHTML = "Date";
-  dateData_element.innerHTML = cards[index].date;
-  venueHeading_element.innerHTML = "Venue";
-  venueData_element.innerHTML = cards[index].venue;
-  locationHeading_element.innerHTML = "Location";
-  locationData_element.innerHTML = cards[index].location;
-  cardButton_element.innerHTML = "Buy Tickets";
+  const dateHeading_element = document.createElement("h2");
+  dateHeading_element.innerText = "Date";
+  const dateData_element = document.createElement("p");
+  // we are setting the <p></p> innerText to card.date.
+  dateData_element.innerText = `${card.date.toLocaleDateString()}`;
+
+  const venueHeading_element = document.createElement("h2");
+  venueHeading_element.innerText = "Venue";
+  const venueData_element = document.createElement("p");
+  // we are setting the <p></p> element into card.venue.
+  // card is an object that the function requires when you call it.
+  venueData_element.innerText = card.venue;
+
+  const locationHeading_element = document.createElement("h2");
+  locationHeading_element.innerText = "Location";
+  const locationData_element = document.createElement("p");
+  locationData_element.innerText = card.location;
+
+  const cardButton_element = document.createElement("button");
+  cardButton_element.innerText = "Buy Tickets";
+
+  const underline_element = document.createElement("hr");
 
   // appends the HTML elements into the card_element (div).
   // for example: from this:  <div></div> ----> to this:  <div><h2>Date</h2></div>
@@ -76,25 +83,17 @@ for (let index = 0; index < cards.length; index++) {
   card_element.appendChild(cardButton_element);
   card_element.appendChild(underline_element);
   // we add a child in the tickets_element (this is the section ID = tickets).
+  // for example. from: <section></section> -----> To: <section><div>...</div></section>
   tickets_element.appendChild(card_element);
 }
 
-// *****This entire forloop ends when index is NOT less than cards.length.*****
-
-// ADDITIONAL NOTES
-
-// // const variable called tickets_element and we're setting this variable into
-// // HTML with the ID #tickets.
-// const tickets_element = document.querySelector("#tickets");
-
-// // creating a forloop. at the start of the loop, we create an index and setting it to 0.
-// // the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
-
-// for (let index = 0; index < cards.length; index++) {
-// // for everyloop, we are creating a div HTML element and we're storing it in a variable called card_element.
-//   const card_element = document.createElement("div");
-// // we set the contents of the card_element into the individual items of the cards list.
-//   card_element.innerHTML = cards[index].venue;
-// // we add a child in the tickets_element (this is the section ID = tickets).
-//   tickets_element.appendChild(card_element);
-// }
+// creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
+// the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
+for (let index = 0; index < cards.length; index++) {
+  // for every loop, we call the createCard function.
+  // we call the createCard function created above.
+  // the card parameter is (cards[index]) and we are accessing [index].
+  // for example, cards[0] -----> cards[1] -----> cards[2] ...
+  // this is how you access a single card in the cards array.
+  createCard(cards[index]);
+}
