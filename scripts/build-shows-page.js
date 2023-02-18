02327dde709deb5aa360556651de89bd9a4b5df4
gjpineda <gjpineda@live.com>
2023-02-18 12:10:48 -0800

    reformatted entire shows js and bio js.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,7 +1,9 @@
-// The square brackets signify an array. We are creating a const variable called "cards".
+// The square brackets signify an array. We are creating a const variable called "shows".
 // and we are setting it to an array.
 // for each array item, we make an object. curly braces signify object.
 // each object, has the property called date, venue, and location.
+// key:value pair
+// Date is the key, and "Mon Sept 06 2021" is the value.
 const shows = [
   {
     date: "Mon Sept 06 2021",
@@ -35,6 +37,8 @@ const shows = [
   },
 ];
 
+// -----------------------------------------
+
 // the reason why we store this in a variable is so we can access it later. "ticketsElement" is a VARIABLE.
 // we are storing the HTML element with the ID "#tickets" into ticketsElement VARIABLE.
 // the document.querySelector selects an HTML element with ID "#tickets" then
@@ -43,57 +47,103 @@ const shows = [
 // a hashtag makes something an ID.
 const ticketsElement = document.querySelector("#tickets");
 
-// We created a reusable function that requires a parameter called "card".
-// "card" is expected to be an object with the properties: Date, Venue, and Location.
-function createCard(card) {
-  // everytime this function is called, we are creating an element called "div, h2, p, etc."
-  const cardElement = document.createElement("div");
+// The createTable function creates this ---> <section><table><tr><th></th></tr></table></section>
+function createTable() {
+  // This creates a table element: <table></table>
+  const tableElement = document.createElement("table");
+  tableElement.id = "ticketsTable";
 
-  const dateHeadingElement = document.createElement("h2");
-  dateHeadingElement.innerText = "Date";
-  const dateDataElement = document.createElement("p");
-  // we are setting the <p></p> innerText to card.date.
-  dateDataElement.innerText = card.date;
+  // This creates a row element: <tr></tr>
+  const rowElement = document.createElement("tr");
 
-  const venueHeadingElement = document.createElement("h2");
-  venueHeadingElement.innerText = "Venue";
-  const venueDataElement = document.createElement("p");
-  // we are setting the <p></p> element into card.venue.
-  // card is an object that the function requires when you call it.
-  venueDataElement.innerText = card.venue;
+  // This creates 3 headers elements.
+  const headerElement1 = document.createElement("th"); // <th></th>
+  const headerElement2 = document.createElement("th"); // <th></th>
+  const headerElement3 = document.createElement("th"); // <th></th>
 
-  const locationHeadingElement = document.createElement("h2");
-  locationHeadingElement.innerText = "Location";
-  const locationDataElement = document.createElement("p");
-  locationDataElement.innerText = card.location;
+  // This Changes the innerText of each headers to Date, Venue, Location
+  headerElement1.innerText = "Date"; // <th>Date</th>
+  headerElement2.innerText = "Venue"; // <th>Venue</th>
+  headerElement3.innerText = "Location"; // <th>Location</th>
 
-  const cardButtonElement = document.createElement("button");
-  cardButtonElement.innerText = "Buy Tickets";
+  // This adds the headers element to the row element
+  rowElement.appendChild(headerElement1); // <tr><th></th></tr>
+  rowElement.appendChild(headerElement2); // <tr><th></th></tr>
+  rowElement.appendChild(headerElement3); // <tr><th></th></tr>
 
-  const underlineElement = document.createElement("hr");
+  // This adds the row to the table.
+  tableElement.appendChild(rowElement); // <table><tr><th></th></tr></table>
 
-  // appends the HTML elements into the cardElement (div).
-  // for example: from this:  <div></div> ----> to this:  <div><h2>Date</h2></div>
-  cardElement.appendChild(dateHeadingElement);
-  cardElement.appendChild(dateDataElement);
-  cardElement.appendChild(venueHeadingElement);
-  cardElement.appendChild(venueDataElement);
-  cardElement.appendChild(locationHeadingElement);
-  cardElement.appendChild(locationDataElement);
-  cardElement.appendChild(cardButtonElement);
-  cardElement.appendChild(underlineElement);
-  // we add a child in the ticketsElement (this is the section ID = tickets).
-  // for example. from: <section></section> -----> To: <section><div>...</div></section>
-  ticketsElement.appendChild(cardElement);
+  // This adds the entire table to the ticketsElement section
+  ticketsElement.appendChild(tableElement); // <section><table><tr><th></th></tr></table></section>
 }
 
+// -----------------------------------------
+
+// We created a reusable function which is "createTableRow" that requires a parameter called "card".
+// "card" is expected to be an object with the properties: Date, Venue, and Location.
+// "card" is a parameter/argument.
+function createTableRow(card) {
+  // everytime this function is called, we are creating an element called "tr, td"
+  // This is an example of what it looks like: <table id="ticketsTable"></table>
+  const ticketsTableElement = document.querySelector("#ticketsTable");
+
+  // This creates a <tr></tr>
+  const rowDataElement = document.createElement("tr");
+
+  // This creates a <td></td>
+  const dateDataElement = document.createElement("td");
+  // The top becomes this <td></td> -----> to this <td>Mon Sept 06 2021</td>
+  dateDataElement.innerText = card.date;
+
+  // This creates a <td></td>
+  const venueDataElement = document.createElement("td");
+  // The top becomes this <td></td> -----> to this <td>Ronald Lane</td>
+  venueDataElement.innerText = card.venue;
+
+  // This creates a <td></td>
+  const locationDataElement = document.createElement("td");
+  // The top becomes <td></td> -----> This <td>San Francisco, CA</td>
+  locationDataElement.innerText = card.location;
+
+  // This creates a <button></button>
+  const cardButtonElement = document.createElement("button");
+  // The top becomes <button></button> -----> This <button>Buy Tickets</button>
+  cardButtonElement.innerText = "Buy Tickets";
+  
+  // This appends dateDataElement to rowDataElement
+  // <tr></tr> -----> <tr><td>Mon Sept 06 2021</td></tr>
+    /* First it's this:
+    <tr><td>Mon Sept 06 2021</td></tr> ---->
+
+      Then it becomes this:
+    <tr>
+      <td>Mon Sept 06 2021</td>
+      <td>Ronald Lane</td>
+    </tr>
+  */
+  rowDataElement.appendChild(dateDataElement);
+  rowDataElement.appendChild(venueDataElement);
+  rowDataElement.appendChild(locationDataElement);
+
+  // This appends rowDataElement to ticketsTableElement.
+  // This: <table id="ticketsTable"></table> ----> Becomes this: <table id="ticketsTable"><tr>...</tr></table>
+  ticketsTableElement.appendChild(rowDataElement);
+}
+
+// ------------------------------------------------------------------------------------------
+
+// this calls the function "createTable()"
+// the reason why it's at the bottom is for organizational purposes.
+createTable();
+
 // creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
-// the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
+// the loop keeps going if index is < shows.length. for everyloop, we increment index by 1 (index++)
 for (let index = 0; index < shows.length; index++) {
-  // for every loop, we call the createCard function.
-  // we call the createCard function created above.
-  // the card parameter is (cards[index]) and we are accessing [index].
-  // for example, cards[0] -----> cards[1] -----> cards[2] ...
-  // this is how you access a single card in the cards array.
-  createCard(shows[index]);
+  // for every loop, we call the createTableRow function.
+  // we call the createTableRow function created above. (line 82)
+  // the card parameter is (shows[index]) and we are accessing [index].
+  // for example, shows[0] -----> shows[1] -----> shows[2] ...
+  // this is how you access a single card in the shows array.
+  createTableRow(shows[index]);
 }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,7 +1,9 @@
-// The square brackets signify an array. We are creating a const variable called "cards".
+// The square brackets signify an array. We are creating a const variable called "commentListObject".
 // and we are setting it to an array.
 // for each array item, we make an object. curly braces signify object.
-// each object, has the property called date, venue, and location.
+// each object, has the property called name, date, and comment.
+// key:value pair
+// Date is the key, and "new Date(1676620800000)" is the value.
 const commentListObject = [
   {
     name: "Connor Walton",
@@ -31,22 +33,22 @@ const commentListObject = [
 // a hashtag makes something an ID.
 const commentListElement = document.querySelector("#comments");
 
-// We created a reusable function that requires a parameter called "card".
-// "card" is expected to be an object with the properties: Date, Venue, and Location.
-function createCard(card) {
+// We created a reusable function that requires a parameter called "comment".
+// "comment" is expected to be an object with the properties: Date, Venue, and Location.
+function displayComment(comment) {
   // everytime this function is called, we are creating an element called "div, h2, p, etc."
   const underlineElement = document.createElement("hr");
 
   const cardElement = document.createElement("div");
 
   const nameElement = document.createElement("h2");
-  nameElement.innerText = card.name;
+  nameElement.innerText = comment.name;
 
   const dateElement = document.createElement("p");
-  dateElement.innerText = `${card.date.toLocaleDateString()}`;
+  dateElement.innerText = `${comment.date.toLocaleDateString()}`;
 
   const commentElement = document.createElement("p");
-  commentElement.innerText = card.comment;
+  commentElement.innerText = comment.comment;
 
   // appends the HTML elements into the cardEelement (div).
   // for example: from this:  <div></div> ----> to this:  <div><h2>Date</h2></div>
@@ -58,13 +60,87 @@ function createCard(card) {
   commentListElement.appendChild(cardElement);
 }
 
-// creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
-// the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
-for (let index = 0; index < commentListObject.length; index++) {
-  // for every loop, we call the createCard function.
-  // we call the createCard function created above.
-  // the card parameter is (cards[index]) and we are accessing [index].
-  // for example, cards[0] -----> cards[1] -----> cards[2] ...
-  // this is how you access a single card in the cards array.
-  createCard(commentListObject[index]);
+function resetCommentsList() {
+  // 1. Select the <section> which is in id=comments.
+  const commentListReset = document.querySelector("#comments");
+  // 2. empty the section.
+  // the commentReset.innerHTML sets the section to an empty variable.
+  // For example: From: <section>..childrens..</section> ---> To: <section></setcion>
+  commentListReset.innerHTML = "";
 }
+
+// The reason why we created a createCards function is to make it reusable
+// we want to sort it multiple times (everytime we add a new data)
+// reset <section> into empty.
+function createCards() {
+  // Resets the commentsList
+  // this calls the resetCommentsList
+  resetCommentsList();
+
+  // I am sorting here.
+  // commentListObject array is being sorted using "sort" function
+  // Eveytime you use something() <-- () is a function.
+  // (a,b) => b.date - a.date will sort from recent to least recent.
+  commentListObject.sort((a, b) => b.date - a.date);
+
+  // creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
+  // the loop keeps going if index is < cards.length. for everyloop, we increment index by 1 (index++)
+  for (let index = 0; index < commentListObject.length; index++) {
+    // for every loop, we call the createCard function.
+    // we call the createCard function created above.
+    // the card parameter is (cards[index]) and we are accessing [index].
+    // for example, cards[0] -----> cards[1] -----> cards[2] ...
+    // this is how you access a single card in the cards array.
+    displayComment(commentListObject[index]);
+  }
+}
+
+// This is calling the createCards function in line 63
+createCards();
+
+// 1. Find the form HTML element.
+const formCommentElement = document.querySelector("#formComment");
+// 2. Add functionality/event on it if you click submit.
+formCommentElement.addEventListener("submit", (event) => {
+  // Prevents from refreshing the page.
+  event.preventDefault();
+
+  // 3. Access the name and comment
+  const nameInput = event.target.name.value;
+  const commentInput = event.target.comment.value;
+  // event is referring to the "mouse click"
+  // event.target is referring to the "form" What is the target of what you clicked? the form.
+  // event.target.name is referring to the HTML element with the name=name, or name=comment
+  // event.target.name.value is referring to the user input/value (what you entered in the box).
+
+  // 4. Access the date
+  const dateInput = Date.now();
+  // everytime there is an = sign, you are storing something into a VARIABLE.
+
+  // 5. create an object out of them
+  const newComment = {
+    name: nameInput,
+    date: new Date(dateInput),
+    comment: commentInput,
+  };
+  // I took the nameInput, dateInput, and commentInput and stored it to newComment.
+
+  // 6. Add the new object to the array/list of comments
+  commentListObject.push(newComment);
+  // commentListObject is the original array of default comments at the very top.
+  // push adds it to the array, but at the bottom.
+  // newComment is the new name, date, comment that a user inputs.
+
+  // createCard calls the createCard function we created above. (line 38)
+  // the card parameter is (newComment) because we need to pass
+  // the data (newComment line 94) to the createCard function above.
+
+  // This is calling the createCards function in line 63
+  // this makes createCards REUSABLE
+  createCards();
+
+  // event.target is referring to the "form".
+  // event.target.RESET, CLEARS THE ENTIRE FORM.
+  // There's an empty bracket because it's a function without a parameter
+  event.target.reset();
+});
