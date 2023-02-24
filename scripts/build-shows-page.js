fd583aeddbd84788e6e0e27ca977b182ae7c064f
gjpineda <gjpineda@live.com>
2023-02-23 20:27:12 -0800

    reduced comments + shows API + Like button

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,50 +1,48 @@
-// The square brackets signify an array. We are creating a const variable called "shows".
-// and we are setting it to an array.
-// for each array item, we make an object. curly braces signify object.
-// each object, has the property called date, venue, and location.
-// key:value pair
-// Date is the key, and "Mon Sept 06 2021" is the value.
-const shows = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21, 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15, 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+let apiVariable = "";
+let shows = [];
+
+// This is the table element and I am storing it in a variable to use later.
+const ticketsTableElement = document.querySelector("#ticketsTable");
+
+// We are now creating a function to set the apiVariable to response.data.api_key.
+function apiKey() {
+  axios
+    .get("https://project-1-api.herokuapp.com/register")
+    .then(function (response) {
+      // we're getting the response, and storing it in a variable called "apiVariable", which is also above.
+      apiVariable = response.data.api_key;
+      // we are calling getComments, which is below.
+      getShows();
+    });
+}
+
+function getShows() {
+  axios
+    .get(`https://project-1-api.herokuapp.com/showdates?api_key=${apiVariable}`)
+    .then(function (response) {
+      shows = response.data;
+      console.log(response.data);
+      // this calls the function "createTable()"
+      createTable();
+      createShows();
+    });
+}
+
+function createShows() {
+  // I am sorting here.
+  shows.sort((a, b) => b.date - a.date);
+  // creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
+  for (let index = 0; index < shows.length; index++) {
+    // for every loop, we call the createTableRow function.
+    createTableRow(shows[index]);
+  }
+}
+
+apiKey();
 
 // -----------------------------------------
 
-// the reason why we store this in a variable is so we can access it later. "ticketsElement" is a VARIABLE.
-// we are storing the HTML element with the ID "#tickets" into ticketsElement VARIABLE.
-// the document.querySelector selects an HTML element with ID "#tickets" then
-// stores it into the ticketsElement VARIABLE.
 // querySelector is a general selector, others include getElementByID, get ElementByClass, etc.
-// a hashtag makes something an ID.
 const ticketsElement = document.querySelector("#tickets");
 
 // The createTable function creates this ---> <section><table><tr><th></th></tr></table></section>
@@ -52,22 +50,16 @@ function createTable() {
   // This creates a table element: <table></table>
   const tableElement = document.createElement("table");
   tableElement.id = "ticketsTable";
-
-  // This adds the entire table to the ticketsElement section
-  ticketsElement.appendChild(tableElement); // <section><table><tr><th></th></tr></table></section>
+  // <section><table><tr><th></th></tr></table></section>
+  ticketsElement.appendChild(tableElement);
 }
 
 // -----------------------------------------
 
 // We created a reusable function which is called "createTableRow" that requires a parameter called "card".
-// "card" is expected to be an object with the properties: Date, Venue, and Location.
-// "card" is a parameter/argument.
 function createTableRow(card) {
-  // every time this function is called, we are creating an element called "tr, td"
   // This is an example of what it looks like: <table id="ticketsTable"></table>
   const ticketsTableElement = document.querySelector("#ticketsTable");
-
-  // THIS ENTIRE BUTTON SECTION IS FOR THE BUTTON SECTION OF THE MOBILE
 
   // This creates a "button" element. <button></button>
   const buttonElement = document.createElement("button");
@@ -81,9 +73,6 @@ function createTableRow(card) {
   const buttonElement2 = document.createElement("button");
   buttonElement2.innerText = "Buy Tickets";
   buttonElement2.classList.add("ticket__button");
-
-  // Note: The reason for two button elements is because you cannot use "buttonElement" twice.
-  // therefore, I made a buttonElement and buttonElement2.
 
   // This creates a <tr></tr>
   const rowDataElement1 = document.createElement("tr");
@@ -134,12 +123,24 @@ function createTableRow(card) {
   // This creates a <td></td>
   const dataElement1 = document.createElement("td");
   // This puts all the data in card inside td. <td>Mon Sept 06 2021<td>
-  dataElement1.innerText = card.date;
+  const day = new Date(card.date);
+
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  // The replaceAll function would remove the comma "," and replace it with an empty space " ".
+  const formattedDate = day.toLocaleString("en-US", options).replaceAll(",", " ");
+
+  dataElement1.innerText = formattedDate;
+
   // This adds css to the td. <td class="ticket__date">Mon Sept 06 2021</td>
   dataElement1.classList.add("ticket__date");
 
   const dataElement2 = document.createElement("td");
-  dataElement2.innerText = card.venue;
+  dataElement2.innerText = card.place;
   dataElement2.classList.add("ticket__row");
   dataElement2.classList.add("ticket__content");
 
@@ -153,7 +154,7 @@ function createTableRow(card) {
   dataElement4.classList.add("ticket__row");
 
   const dataElement5 = document.createElement("td");
-  dataElement5.innerText = card.venue;
+  dataElement5.innerText = card.place;
   dataElement5.classList.add("ticket__content");
 
   const dataElement6 = document.createElement("td");
@@ -168,30 +169,16 @@ function createTableRow(card) {
 
   // This puts rowDataElement1 inside headerElement1. <tr><th></th></tr>
   rowDataElement1.appendChild(headerElement1);
-  // This puts rowDataElement2 inside headerElement1. <tr><th></th></tr>
   rowDataElement1.appendChild(headerElement2);
-  // This puts rowDataElement3 inside headerElement1. <tr><th></th></tr>
   rowDataElement1.appendChild(headerElement3);
-
-  // This puts dataElement1 inside rowDataElement2. <tr><td></td></tr>
   rowDataElement2.appendChild(dataElement1);
-  // This puts dataElement2 inside rowDataElement2. <tr><td></td></tr>
   rowDataElement2.appendChild(dataElement2);
-  // This puts dataElement3 inside rowDataElement2. <tr><td></td></tr>
   rowDataElement2.appendChild(dataElement3);
-  // This puts dataElement4 inside rowDataElement2. <tr><td></td></tr>
   rowDataElement2.appendChild(dataElement4);
-
-  // This puts headerElement4 inside rowDataElement3. <tr><th></th></tr>
   rowDataElement3.appendChild(headerElement4);
-  // This puts dataElement5 inside rowDataElement4. <tr><td></td></tr>
   rowDataElement4.appendChild(dataElement5);
-
-  //  This puts headerElement5 inside rowDataElement5. <tr><th></th></tr>
   rowDataElement5.appendChild(headerElement5);
-  // This puts dataElement6 inside rowDataElement6. <tr><td></td></tr>
   rowDataElement6.appendChild(dataElement6);
-  // This puts dataElement7 inside rowDataElement7. <tr><td></td></tr>
   rowDataElement7.appendChild(dataElement7);
 
   // This puts rowDataElement1 inside ticketsTableElement. <table><tr></tr></table>
@@ -205,23 +192,6 @@ function createTableRow(card) {
 }
 
 // ------------------------------------------------------------------------------------------
-
-// this calls the function "createTable()"
-// the reason why it's at the bottom is for organizational purposes.
-createTable();
-
-// creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
-// the loop keeps going if index is < shows.length. for everyloop, we increment index by 1 (index++)
-for (let index = 0; index < shows.length; index++) {
-  // for every loop, we call the createTableRow function.
-  // we call the createTableRow function created above. (line 82)
-  // the card parameter is (shows[index]) and we are accessing [index].
-  // for example, shows[0] -----> shows[1] -----> shows[2] ...
-  // this is how you access a single card in the shows array.
-  createTableRow(shows[index]);
-}
-// This is the table element and I am storing it in a variable to use later.
-const ticketsTableElement = document.querySelector("#ticketsTable");
 
 function resetAllTrTest() {
   for (let index = 0; index < ticketsTableElement.rows.length; index++) {
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,47 +1,16 @@
 // The square brackets signify an array. We are creating a const variable called "commentListObject".
-// and we are setting it to an array.
 let commentListObject = [];
-
-/* Goal: Display data from API
-0. Create a function that generates the API key and store it as a variable.
-1. Create a function to get the data from the API
-2. store the data into a variable.
-3. use the variable to display the data.*/
-
-// notes:CDN is a storage for JS code. It could also be used for CSS code. CDN = storage.
-// Axios is a library that helps us use URLs. URLs can also be APIs.
-
 // This is setting the api_key to a variable called "apiVariable". We use "let" because the API key always changes.
-// we are setting it to a blank string so it could be filled with the api_key we are setting in the apiKey function.
 let apiVariable = "";
 
-// We are using the library called "axios" to use the API.
-// The "get" is the HTTP METHOD.
-// HTTP METHOD:
-// -get
-// -post
-// -put
-// -delete
 // We are now creating a function to set the apiVariable to response.data.api_key.
 function apiKey() {
   axios
-    // The response from the api comes from the link at the bottom.
-    // The link at the bottom is an api.
     .get("https://project-1-api.herokuapp.com/register")
-    // "Then" is an example of a "promise".
-    // When the link above gives us a response, we do the function inside "then."
-    // When the link above finishes loading, THEEENNNN we do the following...
     .then(function (response) {
-      // Here, we're getting the response, and storing it in a variable called "apiVariable", which is also above.
-      // the reason why its response.data.api_key is because the API key string is INSIDE api_key which is INSIDE data.
-      /*
-        data = {
-          api_key: "this-is-the-api-key"
-        }
-      */
+      // we're getting the response, and storing it in a variable called "apiVariable", which is also above.
       apiVariable = response.data.api_key;
       // we are calling getComments, which is below.
-      // because once we have the api keys, we cant to be able to get the comments using it.
       getComments();
     });
 }
@@ -49,35 +18,24 @@ function apiKey() {
 apiKey();
 
 // This is the same as above but instead of getting the api key, we are getting the data.
-// We are getting the data using the API link.
 function getComments() {
   axios
     .get(`https://project-1-api.herokuapp.com/comments?api_key=${apiVariable}`)
     .then(function (res) {
       commentListObject = res.data;
-      // The createcards is here because it's used to refresh the HTML.
-      // After we get the data, we want to be able to create an HTML with it.
-      // This will display the comments in the html.
+      // The createCards is here because it's used to refresh the HTML.
       createCards();
     });
 }
 
-// the reason why we store this in a variable is so we can access it later. "commentList" is a VARIABLE.
 // we are storing the HTML element with the ID "#comments" into ticketsElement VARIABLE.
-// the document.querySelector selects an HTML element with ID "#comments" then
-// stores it into the "commentList" VARIABLE.
-// querySelector is a general selector, others include getElementByID, get ElementByClass, etc.
-// a hashtag makes something an ID.
 const commentListElement = document.querySelector("#comments");
 
 // We created a reusable function that requires a parameter called "comment".
-// "comment" is expected to be an object with the properties: Date, Venue, and Location.
-// This function creates the HTML for a single comment.
 function displayComment(comment) {
   // every time this function is called, we are creating an element called "div, h2, p, etc."
   const cardContainerElement = document.createElement("div");
   // the classList.add("comment") adds a css class to the div.
-  // for example: <div class=comment></div>
   cardContainerElement.classList.add("comment");
 
   const cardPlaceHolderElement = document.createElement("img");
@@ -96,11 +54,6 @@ function displayComment(comment) {
   const dateElement = document.createElement("p");
 
   // This uses string interpolation to add the comment.timestamp to HTML.
-  // The "timestamp" comes from the name "timestamp" in the array of the API.
-  // it was named "timestamp" instead of "date", which is why I had to change it.
-  // we also needed to convert this into a date object by doing "new Date (comment.timestamp)" because
-  // the timestamp is just numbers. We need to convert this number into a date, hence, new Date.
-  // lastly, toLocaleDateString formats the date.
   dateElement.innerText = `${new Date(comment.timestamp).toLocaleDateString()}`;
   dateElement.classList.add("comment__date");
 
@@ -108,18 +61,27 @@ function displayComment(comment) {
   commentElement.innerText = comment.comment;
   commentElement.classList.add("comment__statement");
 
+  const likeElement = document.createElement("button");
+  likeElement.innerText = "Like";
+  likeElement.classList.add("comment__like-button");
+  likeElement.id = "likeButton";
+  // This links to line 116
+  likeElement.value = comment.id;
+  const likeCount = document.createElement("p");
+  likeCount.innerText = comment.likes;
+
   // This appends the HTML elements into the cardElement (div).
-  // for example: from this:  <div></div> ----> to this:  <div><h2>Date</h2></div>
   nameDateContainer.appendChild(nameElement);
   nameDateContainer.appendChild(dateElement);
 
   cardElement.appendChild(nameDateContainer);
   cardElement.appendChild(commentElement);
+  cardElement.appendChild(likeElement);
+  cardElement.appendChild(likeCount);
 
   cardContainerElement.appendChild(cardPlaceHolderElement);
   cardContainerElement.appendChild(cardElement);
   // we add a child in the ticketsElement (this is the section ID = comments).
-  // commentListElement = <section id="comments"></section>
   commentListElement.appendChild(cardContainerElement);
 }
 
@@ -128,116 +90,103 @@ function resetCommentsList() {
   // 1. Select the <section> which is in id=comments.
   const commentListReset = document.querySelector("#comments");
   // 2. empty the section.
-  // the commentReset.innerHTML sets the section to an empty variable.
-  // For example: From: <section>..children..</section> ---> To: <section></section>
   commentListReset.innerHTML = "";
 }
 
 // The reason why we created a createCards function is to make it reusable
-// we want to sort it multiple times (every time we add a new data)
 function createCards() {
   // reset <section> into empty.
-  // Resets the commentsList
-  // this calls the resetCommentsList
   resetCommentsList();
 
   // I am sorting here.
-  // commentListObject array is being sorted using "sort" function
-  // every time you use xyz() <-- () is a function.
-  // "(a,b) => b.date - a.date" will sort from recent to least recent.
   commentListObject.sort((a, b) => b.timestamp - a.timestamp);
 
   // creating a forloop. at the start of the loop, we create a VARIABLE called "index" and setting it to 0.
-  // the loop keeps going if index is < cards.length. for every loop, we increment index by 1 (index++)
   for (let index = 0; index < commentListObject.length; index++) {
     // for every loop, we call the createCard function.
-    // we call the createCard function created above.
-    // the card parameter is (cards[index]) and we are accessing [index].
-    // for example, cards[0] -----> cards[1] -----> cards[2] ...
-    // this is how you access a single card in the commenListObject array.
     displayComment(commentListObject[index]);
   }
+  buttonsAddEventListeners();
 }
 
-// This is calling the createCards function.
-// Ctrl + click to find createCards.
-createCards();
+// This is the like button functionality
+function buttonsAddEventListeners() {
+  const buttonElements = document.querySelectorAll(".comment__like-button");
+  for (let i = 0; i < buttonElements.length; i++) {
+    buttonElements[i].addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const id = event.target.value;
+
+      axios
+        .put(
+          `https://project-1-api.herokuapp.com/comments/${id}/like?api_key=${apiVariable}`
+        )
+        .then(function (result) {
+          // Find the comment in the array with the specified ID.
+          for (let f = 0; f < commentListObject.length; f++) {
+            if (commentListObject[f].id === id) {
+              // Replace that with the result.
+              commentListObject[f].likes = result.data.likes;
+              // This re-renders (refreshes) the HTML
+              createCards();
+            }
+          }
+        });
+    });
+  }
+}
 
 // 1. Find the form HTML element.
 const formCommentElement = document.querySelector("#formComment");
 // 2. Add functionality/event on it if you click submit.
-// The word "event" in the addEventListener, doesn't have to be event. it could be any other word. ex. "whatever"
 formCommentElement.addEventListener("submit", (event) => {
   // Prevents from refreshing the page.
   event.preventDefault();
 
   // This will reset the errors.
-  // the "remove" function will clear the specified class in the parameter.
   event.target.name.classList.remove("form__error-state");
   event.target.comment.classList.remove("form__error-state");
 
   // 3. Access the name and comment
-  // event is referring to the "mouse click"
-  // event.target is referring to the "form". The target of what you clicked is the form.
-  // event.target.name is referring to the HTML element with the name=name, or name=comment
-  // event.target.name.value is referring to the user input/value (what you entered in the box).
   const nameInput = event.target.name.value;
   const commentInput = event.target.comment.value;
 
-  // THIS IS THE VALIDATION SECTION with Ifs statements
+  // THIS IS THE VALIDATION SECTION
 
-  // "||" means "or"
-  // "&&" means "and"
   // Check if the user inputted BOTH name and comment.
   if (nameInput === "" || commentInput === "") {
     // That means theres an error.
 
     // Where exactly are the errors? Is it the name OR comment OR both?
     if (nameInput === "") {
-      // event is referring to the "mouse click"
-      // event.target is referring to the "form". The target of what you clicked is the form.
-      // event.target.name is referring to the HTML element with the name=name, or name=comment
       // event.target.name.value is referring to the user input/value (what you entered in the box).
       event.target.name.classList.add("form__error-state");
     }
     if (commentInput === "") {
       event.target.comment.classList.add("form__error-state");
     }
-    // Put .error css class in the input
-    // returns ends the entire remaining function below.
-    // having the return here, ignores the rest of the function below IF the condition is met.
     // if nameInput and commentInput is NOT empty, the rest of the function below will execute.
-    return;
+  } else {
+    const newComment = {
+      name: nameInput,
+      comment: commentInput,
+    };
+
+    // we are posting the user inputs into the API here.
+    axios
+      .post(
+        `https://project-1-api.herokuapp.com/comments?api_key=${apiVariable}`,
+        newComment
+      )
+      .then(function (result) {
+        // The api generates timestamp and id from the posted newComment variable
+        commentListObject.push(result.data);
+        // This re-renders the commentListObject array.
+        createCards();
+      });
+
+    // event.target.RESET, CLEARS THE ENTIRE FORM.
+    event.target.reset();
   }
-
-  //----------------------------------
-
-  // This is what we are pulling from the user inputs.
-  // I took the nameInput and commentInput and stored it to newComment.
-  // I added the new object to the array/list of comments
-  const newComment = {
-    name: nameInput,
-    comment: commentInput,
-  };
-
-  // we are posting the user inputs into the API here:
-  // more specifically, the variable newComment.
-  axios
-    .post(
-      `https://project-1-api.herokuapp.com/comments?api_key=${apiVariable}`,
-      newComment
-    )
-    .then(function (result) {
-      // The api generates timestamp and id from the posted newComment variable
-      // and gives it back to us through result.data.
-      commentListObject.push(result.data);
-      // This re-renders the commentListObject array.
-      // It essentially refreshes the HTML.
-      createCards();
-    });
-
-  // event.target is referring to the "form".
-  // event.target.RESET, CLEARS THE ENTIRE FORM.
-  // There's an empty bracket because it's a function without a parameter
-  event.target.reset();
 });
